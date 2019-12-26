b28125a00a54bd780c2f6c8ca958fa86221069ed
Peyman Teymoori <peymant@ifi.uio.no>
2019-12-26 11:07:57 +0100

    ABC cc.

---
+++ inet/src/linklayer/queue/ABCQueue.cc
@@ -0,0 +1,164 @@
+//
+// Copyright (C) 2005 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+
+#include "INETDefs.h"
+#include <algorithm>
+#include "ABCQueue.h"
+#include "PPP.h"
+#include "IPv4Datagram.h"
+
+Define_Module(ABCQueue);
+
+simsignal_t ABCQueue::queueLengthSignal = registerSignal("queueLength");
+simsignal_t ABCQueue::drainRateSignal = registerSignal("drainRate");
+simsignal_t ABCQueue::targetRateSignal = registerSignal("targetRate");
+simsignal_t ABCQueue::fSignal = registerSignal("f");
+
+void ABCQueue::initialize()
+{
+    PassiveQueueBase::initialize();
+
+    queue.setName(par("queueName"));
+
+    //statistics
+    emit(queueLengthSignal, queue.length());
+
+    outGate = gate("out");
+
+    // configuration
+    frameCapacity = par("frameCapacity");
+
+    no = par("no");
+    delayThreshlod = par("delayThreshlod");
+    delta = par("delta");
+    interval = par("interval");
+
+    drainRate = 1;
+    drainRateUpdated = 0;
+    sentLength = 0;
+    token = 0;
+    tokenLimit = 5;
+    duration = 0;
+
+}
+
+cMessage *ABCQueue::enqueue(cMessage *msg)
+{
+    if (frameCapacity && queue.length() >= frameCapacity)
+    {
+        EV << "Queue full, dropping packet.\n";
+        return msg;
+    }
+    else
+    {
+        queue.insert(msg);
+        emit(queueLengthSignal, queue.length());
+        return NULL;
+    }
+}
+
+cMessage *ABCQueue::dequeue()
+{
+    if (queue.empty())
+        return NULL;
+
+    cMessage *msg = (cMessage *)queue.pop();
+
+    // statistics
+    emit(queueLengthSignal, queue.length());
+
+    return msg;
+}
+
+void ABCQueue::requestPacket()
+{
+    Enter_Method("requestPacket()");
+
+    cMessage *msg = dequeue();
+    if (msg == NULL)
+    {
+        packetRequested++;
+    }
+    else
+    {
+        emit(dequeuePkSignal, msg);
+        emit(queueingTimeSignal, simTime() - msg->getArrivalTime());
+
+        double lastQueuingDelay = simTime().dbl() - msg->getArrivalTime().dbl();
+        PPP *ppp = dynamic_cast<PPP *>(getModuleByPath("^.ppp"));
+        if(ppp != NULL) {
+            linkRate = ppp->getDataRate();
+            if(drainRate == 0)
+                drainRate = linkRate;
+        } else
+            linkRate = 1;
+
+        if(simTime() - drainRateUpdated >= interval) {
+            drainRateUpdated = simTime();
+
+
+            drainRate = (sentLength * 8 / interval);// * 0.1 + 0.9 * drainRate;
+//            if(duration > 0)
+//                drainRate = sentLength * 8 / duration;
+            sentLength = 0;
+            duration = 0;
+        }
+
+        cPacket *pkt = check_and_cast<cPacket *>(msg);
+        sentLength += pkt->getByteLength();
+
+        double len = pkt->getByteLength();
+        duration += len * 8 / linkRate;
+
+        targetRate = no * linkRate - (linkRate / delta) * (lastQueuingDelay - delayThreshlod);
+
+        double f = 0.5 * targetRate / drainRate;//linkRate;
+
+        f = std::min(1.0, std::max(0.0, f));
+        token += f;
+
+        if(token > tokenLimit)
+            token = tokenLimit;
+
+        IPv4Datagram * ipPacket = check_and_cast<IPv4Datagram*>(pkt);
+
+        if(token > 1) {
+            if(ipPacket->getExplicitCongestionNotification() == 0)
+                token -= 1;
+        } else {
+            ipPacket->setExplicitCongestionNotification(3);
+        }
+
+        emit(drainRateSignal, drainRate);
+        emit(targetRateSignal, targetRate);
+        emit(fSignal, f);
+
+        sendOut(msg);
+    }
+}
+
+void ABCQueue::sendOut(cMessage *msg)
+{
+    send(msg, outGate);
+}
+
+bool ABCQueue::isEmpty()
+{
+    return queue.empty();
+}
+
---
+++ inet/src/linklayer/queue/ABCQueue.h
@@ -0,0 +1,88 @@
+//
+// Copyright (C) 2005 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+
+#ifndef __INET_ABCQUEUE_H
+#define __INET_ABCQUEUE_H
+
+#include "INETDefs.h"
+
+#include "PassiveQueueBase.h"
+
+/**
+ * Drop-front queue. See NED for more info.
+ */
+class INET_API ABCQueue : public PassiveQueueBase
+{
+  protected:
+    // configuration
+    int frameCapacity;
+
+    double targetRate;
+    double no;
+    double delta;
+    double delayThreshlod;
+    double linkRate;
+
+    double drainRate;
+    double interval;
+    double duration;
+
+    simtime_t drainRateUpdated;
+    double sentLength;
+
+    double token, tokenLimit;
+
+
+    // state
+    cQueue queue;
+    cGate *outGate;
+
+    // statistics
+    static simsignal_t queueLengthSignal;
+    static simsignal_t drainRateSignal;
+    static simsignal_t targetRateSignal;
+    static simsignal_t fSignal;
+
+
+  protected:
+    virtual void initialize();
+
+    /**
+     * Redefined from PassiveQueueBase.
+     */
+    virtual cMessage *enqueue(cMessage *msg);
+
+    /**
+     * Redefined from PassiveQueueBase.
+     */
+    virtual cMessage *dequeue();
+
+    virtual void requestPacket();
+
+    /**
+     * Redefined from PassiveQueueBase.
+     */
+    virtual void sendOut(cMessage *msg);
+
+    /**
+     * Redefined from IPassiveQueue.
+     */
+    virtual bool isEmpty();
+};
+
+#endif
