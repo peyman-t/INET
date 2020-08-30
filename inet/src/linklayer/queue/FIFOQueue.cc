53a1e01a8cddffe8343435ebbb82a322ffbdc77f
peyman-t <peymant@ifi.uio.no>
2020-08-30 10:30:32 +0200

    adding a scenario for the revision 2 of ECN-NUM

--- inet/src/linklayer/queue/FIFOQueue.cc
+++ inet/src/linklayer/queue/FIFOQueue.cc
@@ -38,7 +38,7 @@ cMessage *FIFOQueue::enqueue(cMessage *msg)
     cPacket *packet = check_and_cast<cPacket*>(msg);
     queue.insert(packet);
     byteLength += packet->getByteLength();
-    //if (simTime() >= 5)
+    if (simTime() >= 4)
     emit(queueLengthSignal, queue.length());
     return NULL;
 }
@@ -50,7 +50,7 @@ cMessage *FIFOQueue::dequeue()
 
     cPacket *packet = check_and_cast<cPacket*>(queue.pop());
     byteLength -= packet->getByteLength();
-    //if (simTime() >= 5)
+    if (simTime() >= 4)
     emit(queueLengthSignal, queue.length());
     return packet;
 }
